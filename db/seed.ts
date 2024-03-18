fb0ef286dbd3b8102b1b2ab8d3d32f2c6fcb5a75
abelsrzz <abelsuarezmuino04@gmail.com>
2024-03-18 13:22:35 +0100

    Button fix

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,16 @@
-import { File, db } from 'astro:db';
+import { File, db } from "astro:db";
 
 export default async function () {
   await db.insert(File).values([
-    { id: 1, route: 'primero', resume: "Todo el contenido de primero" },
-    { id: 2, route: 'segundo', resume: "Todo el contenido de segundo" },
-  ])
+    {
+      id: 1,
+      route: "primero",
+      resume: "Todo el contenido de primero",
+    },
+    { 
+        id: 2, 
+        route: "segundo", 
+        resume: "Todo el contenido de segundo" 
+    },
+  ]);
 }
