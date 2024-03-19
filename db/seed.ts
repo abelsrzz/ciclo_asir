172c6dbfbd769c85b48cb4a46c58fb386bc2fa4c
abelsrzz <abelsuarezmuino04@gmail.com>
2024-03-19 12:56:35 +0100

    Teoría tema2

--- db/seed.ts
+++ db/seed.ts
@@ -7,16 +7,28 @@ export default async function () {
       path: "primero",
       resume: "Todo el contenido de primero",
     },
-    { 
-        id: 2, 
-        path: "segundo", 
-        resume: "Todo el contenido de segundo" 
+    {
+      id: 2,
+      path: "segundo",
+      resume: "Todo el contenido de segundo",
     },
-    { 
-        id: 3, 
-        path: "/content/primero/bases/tema1/T01_bases_teoria.pdf",
-        subject: "bases",
-        resume: "Teoría Tema 1 (Introducción a las bases de datos)" 
+    {
+      id: 3,
+      path: "/content/primero/bases/tema1/T01_bases_teoria.pdf",
+      subject: "bases",
+      resume: "Teoría Tema 1 (Introducción a las bases de datos)",
+    },
+    {
+      id: 4,
+      path: "/content/primero/bases/tema2/T02_bases_ejercicios_1.pdf",
+      subject: "bases",
+      resume: "Ejercicios Tema 2 (Entidad relación 1)",
+    },    
+    {
+      id: 5,
+      path: "/content/primero/bases/tema2/T02_bases_ejercicios_2.pdf",
+      subject: "bases",
+      resume: "Ejercicios Tema 2 (Entidad relación 2)",
     },
   ]);
 }
