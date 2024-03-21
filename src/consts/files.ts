6f52f415d0bb697c9a935feb7cf1fbb8d0fb7459
abelsrzz <abelsuarezmuino04@gmail.com>
2024-03-21 18:40:10 +0100

    Add files.ts

---
+++ src/consts/files.ts
@@ -0,0 +1,27 @@
+import { type File } from "../../types/File";
+export const FILES: File[] = [
+    {
+        title: "T01_bases_teoria_0",
+        subjectId: "bd",
+        course: "primero",
+        unit: "tema1",
+        ext: "pdf",
+        resume: "Introducción a las bases de datos",
+    },
+    {
+        title: "T02_bases_ejercicios_0",
+        subjectId: "bd",
+        course: "primero",
+        unit: "tema2",
+        ext: "pdf",
+        resume: "Ejercicios de diagrama Entidad Relación",
+    },
+    {
+        title: "T02_bases_ejercicios_1",
+        subjectId: "bd",
+        course: "primero",
+        unit: "tema2",
+        ext: "pdf",
+        resume: "Ejercicios de diagrama Entidad Relación",
+    },
+]
--- src/consts/subjects.ts
+++ src/consts/subjects.ts
@@ -5,65 +5,100 @@ export const SUBJECTS: Subject[] = [
         name: "Implantación de Sistemas Operativos",
 		courseId: "primero",
 		courseName: "Primero de ASIR",
+		files: [
+
+		]
 	},
 	{
         id: "par",
         name: "Planificación y Administración de Redes",
 		courseId: "primero",
 		courseName: "Primero de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "lm",
         name: "Lenguajes de Marcas",
 		courseId: "primero",
 		courseName: "Primero de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "bd",
         name: "Gestión de Bases de Datos",
 		courseId: "primero",
 		courseName: "Primero de ASIR",
+		files: [
+			"T01_bases_teoria_0",
+			"T02_bases_ejercicios_0",
+			"T02_bases_ejercicios_1",
+		]
 	},
 	{
         id: "fh",
         name: "Fundamentos del Hardware",
 		courseId: "primero",
 		courseName: "Primero de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "sri",
         name: "Servicios de Redes e Internet",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "aso",
         name: "Administración de Sistemas Operativos",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "sgbd",
         name: "Sistemas Gestores de Bases de Datos",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "iaw",
         name: "Implantación de Aplicaciones Web",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "sad",
         name: "Seguridad y Alta Disponibilidad",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 	{
         id: "fct",
         name: "Trabajo de Fin de Ciclo",
 		courseId: "segundo",
 		courseName: "Segundo de ASIR",
+		files: [
+			
+		]
 	},
 ]
